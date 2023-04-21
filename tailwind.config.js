c87fac93910a8871dc25b1660e25a6fe3543d3b9
ChaconEfrain <efrachaga@gmail.com>
2023-04-21 12:48:42 -0600

    Add custom colors and fonts

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,31 @@
 /** @type {import('tailwindcss').Config} */
+
 module.exports = {
   content: [
-    './src/pages/**/*.{js,ts,jsx,tsx}',
-    './src/components/**/*.{js,ts,jsx,tsx}',
-    './src/app/**/*.{js,ts,jsx,tsx}',
+    "./src/pages/**/*.{js,ts,jsx,tsx}",
+    "./src/components/**/*.{js,ts,jsx,tsx}",
+    "./src/app/**/*.{js,ts,jsx,tsx}",
   ],
+  plugins: [],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      colors: {
+        primary: {
+          light: "#e7f0f2",
+          medium: "#88b6be",
+          dark: "#445b5f",
+          darkest: "#0e1213",
+        },
+        secondary: {
+          light: "#ffe3e7",
+          medium: "#ff7588",
+          dark: "#803b44",
+          darkest: "#190c0e",
+        },
+      },
+      fontFamily: {
+        alegreya: ["var(--font-alegreya)", "sans-serif"],
       },
     },
   },
-  plugins: [],
-}
+};
