839d15fd620e26ab790e93600eb105c2b3fb6559
ChaconEfrain <efrachaga@gmail.com>
2023-04-21 12:49:02 -0600

    Add custom font

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,16 @@
-import '@/styles/globals.css'
+import "@/styles/globals.css";
+import { Alegreya_Sans } from "@next/font/google";
+
+const alegreya = Alegreya_Sans({
+  subsets: ["latin"],
+  weight: ["400", "500", "700"],
+  variable: "--font-alegreya",
+});
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <div className={`${alegreya.variable} font-alegreya`}>
+      <Component {...pageProps} />
+    </div>
+  );
 }
