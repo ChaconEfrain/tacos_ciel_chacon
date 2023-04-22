60bcd647b2d6e2f73a0139498a3f49edcd6d3ca8
ChaconEfrain <efrachaga@gmail.com>
2023-04-22 12:50:21 -0600

    Add promotions component

--- src/components/Promotion.jsx
+++ src/components/Promotion.jsx
@@ -1,7 +1,15 @@
-import React from "react";
-
-const Promotion = ({ title, tacos, drinks, price }) => {
-  return <article></article>;
+const Promotion = ({ title, description, tacos, drinks, price, icon }) => {
+  return (
+    <article>
+      <h3 className="text-xl flex items-center gap-2">
+        <span className="bg-secondary-medium/50 rounded-full w-10 h-10 flex items-center justify-center shadow">
+          <span className="w-6 h-6">{icon}</span>
+        </span>
+        {title}
+      </h3>
+      <p>{description}</p>
+    </article>
+  );
 };
 
 export default Promotion;
--- src/components/Promotions.jsx
+++ src/components/Promotions.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable @next/next/no-img-element */
 import { promotions } from "@/constants";
 import Container from "./Container";
 import Promotion from "./Promotion";
@@ -5,17 +6,29 @@ import Promotion from "./Promotion";
 const Promotions = () => {
   return (
     <Container>
-      <div>
-        <h2>Promociones</h2>
-        {promotions.map(({ title, tacos, drinks, price }) => (
-          <Promotion
-            key={price}
-            title={title}
-            tacos={tacos}
-            drinks={drinks}
-            price={price}
+      <div className="grid grid-cols-[55fr_45fr] gap-4">
+        <div>
+          <h2>Promociones</h2>
+          {promotions.map(
+            ({ title, description, tacos, drinks, price, icon }) => (
+              <Promotion
+                key={price}
+                title={title}
+                description={description}
+                tacos={tacos}
+                drinks={drinks}
+                price={price}
+                icon={icon}
+              />
+            )
+          )}
+        </div>
+        <figure>
+          <img
+            src="https://res.cloudinary.com/efrainchacon/image/upload/f_auto/g_auto/v1681867756/tacos_de_canasta_ciel_chacon/FB_IMG_1666010222566_081550_1_enykze.jpg"
+            alt="Tacos de canasta con salsa"
           />
-        ))}
+        </figure>
       </div>
     </Container>
   );
