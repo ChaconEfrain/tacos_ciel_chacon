93f9cdc6df6eea2981751b8b4334737cc2507a0e
ChaconEfrain <efrachaga@gmail.com>
2023-04-22 12:49:57 -0600

    Add description to each promotion

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,20 +1,31 @@
+import { Couple, Family, Person } from "@/icons";
+
 export const promotions = [
   {
     title: "Personal",
+    description:
+      "Si buscas algo rápido, con buen sabor y a un excelente precio, la promoción personal es ideal para ti!",
     tacos: "4 tacos",
     drinks: "1 refresco",
     price: "$55",
+    icon: <Person />,
   },
   {
     title: "Pareja",
+    description:
+      "El paquete perfecto para compartir con tu amigo/a, pareja ó con quien tú quieras!",
     tacos: "8 tacos",
     drinks: "2 refrescos",
     price: "$105",
+    icon: <Couple />,
   },
   {
     title: "Familiar",
+    description:
+      "¿No sabes qué cocinar para tu familia? Con la promoción familiar todos quedan satisfechos, hasta tu bolsillo!",
     tacos: "20 tacos",
     drinks: "4 refrescos",
     price: "$205",
+    icon: <Family />,
   },
 ];
