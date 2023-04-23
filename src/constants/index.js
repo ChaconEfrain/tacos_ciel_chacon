4dcfa8e9b7f6123da214130d98d4a8022e93dc63
ChaconEfrain <efrachaga@gmail.com>
2023-04-23 13:44:04 -0600

    Add packages array

--- src/constants/index.js
+++ src/constants/index.js
@@ -29,3 +29,101 @@ export const promotions = [
     icon: <Family />,
   },
 ];
+
+export const packages = [
+  {
+    title: "Pa que pruebes",
+    groupSize: "20 personas",
+    optionOne: {
+      tacos: "80 tacos",
+      tacosForEach: "4 tacos por persona",
+      price: "$960",
+    },
+    optionTwo: {
+      tacos: "100 tacos",
+      tacosForEach: "5 tacos por persona",
+      price: "$1,200",
+    },
+    optionThree: {
+      tacos: "120 tacos",
+      tacosForEach: "6 tacos por persona",
+      price: "$1,440",
+    },
+  },
+  {
+    title: "Pa tu reunión",
+    groupSize: "30 personas",
+    optionOne: {
+      tacos: "120 tacos",
+      tacosForEach: "4 tacos por persona",
+      price: "$1,440",
+    },
+    optionTwo: {
+      tacos: "150 tacos",
+      tacosForEach: "5 tacos por persona",
+      price: "$1,650",
+    },
+    optionThree: {
+      tacos: "180 tacos",
+      tacosForEach: "6 tacos por persona",
+      price: "$1,980",
+    },
+  },
+  {
+    title: "Pa que convivas",
+    groupSize: "40 personas",
+    optionOne: {
+      tacos: "160 tacos",
+      tacosForEach: "4 tacos por persona",
+      price: "$1,760",
+    },
+    optionTwo: {
+      tacos: "200 tacos",
+      tacosForEach: "5 tacos por persona",
+      price: "$2,200",
+    },
+    optionThree: {
+      tacos: "240 tacos",
+      tacosForEach: "6 tacos por persona",
+      price: "$2,640",
+    },
+  },
+  {
+    title: "Pa que festejes",
+    groupSize: "50 personas",
+    optionOne: {
+      tacos: "200 tacos",
+      tacosForEach: "4 tacos por persona",
+      price: "$2,200",
+    },
+    optionTwo: {
+      tacos: "250 tacos",
+      tacosForEach: "5 tacos por persona",
+      price: "$2,500",
+    },
+    optionThree: {
+      tacos: "300 tacos",
+      tacosForEach: "6 tacos por persona",
+      price: "$3,000",
+    },
+  },
+  {
+    title: "Pa que te enfiestes",
+    groupSize: "60 personas",
+    optionOne: {
+      tacos: "240 tacos",
+      tacosForEach: "4 tacos por persona",
+      price: "$2,400",
+    },
+    optionTwo: {
+      tacos: "300 tacos",
+      tacosForEach: "5 tacos por persona",
+      price: "$3,000",
+    },
+    optionThree: {
+      tacos: "360 tacos",
+      tacosForEach: "6 tacos por persona",
+      price: "$3,600",
+    },
+  },
+];
