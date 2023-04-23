690ac44f066c4171d6e496c2821fd174ea9df34a
ChaconEfrain <efrachaga@gmail.com>
2023-04-23 12:19:08 -0600

    Add margins and padding to the container

--- src/components/Container.jsx
+++ src/components/Container.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 const Container = ({ children }) => {
-  return <div className="max-w-[80rem]">{children}</div>;
+  return <div className="max-w-[80rem] mx-auto mt-24 px-3">{children}</div>;
 };
 
 export default Container;
