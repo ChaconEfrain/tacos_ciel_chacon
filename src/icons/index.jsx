915af4f4c363f0136a33c3a7f5e3aceef1983213
ChaconEfrain <efrachaga@gmail.com>
2023-04-24 14:07:00 -0600

    Add Check icon

--- src/icons/index.jsx
+++ src/icons/index.jsx
@@ -260,6 +260,25 @@ export const Soda = ({ className }) => (
   </svg>
 );
 
+export const Check = ({ className }) => (
+  <svg
+    className={className}
+    xmlns="http://www.w3.org/2000/svg"
+    xmlSpace="preserve"
+    width={200}
+    height={200}
+    fill="#ff7588"
+    viewBox="0 0 512 512"
+  >
+    <path
+      d="M469.402 35.492C334.09 110.664 197.114 324.5 197.114 324.5L73.509 184.176 0 254.336l178.732 222.172 65.15-2.504C327.414 223.414 512 55.539 512 55.539l-42.598-20.047z"
+      style={{
+        fill: "#ff7588",
+      }}
+    />
+  </svg>
+);
+
 // export const Money = ({ className }) => (
 //   <svg
 //     xmlns="http://www.w3.org/2000/svg"
