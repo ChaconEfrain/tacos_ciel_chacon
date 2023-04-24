3b56ce14cb671ee115a0ca973f90faddf95b784a
ChaconEfrain <efrachaga@gmail.com>
2023-04-24 14:03:12 -0600

    Change container to be a section

--- src/components/Container.jsx
+++ src/components/Container.jsx
@@ -1,7 +1,9 @@
 import React from "react";
 
 const Container = ({ children }) => {
-  return <div className="max-w-[80rem] mx-auto mt-24 px-3">{children}</div>;
+  return (
+    <section className="max-w-[80rem] mx-auto mt-24 px-3">{children}</section>
+  );
 };
 
 export default Container;
