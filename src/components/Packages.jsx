419e1f654dbd26d9dd9852e8f14af53775c905e8
ChaconEfrain <efrachaga@gmail.com>
2023-04-24 14:04:41 -0600

    Add Menu component to the Packages component

--- src/components/Packages.jsx
+++ src/components/Packages.jsx
@@ -1,36 +1,25 @@
-import { useEffect, useRef } from "react";
+import { useEffect } from "react";
 import { register } from "swiper/element/bundle";
 import Container from "./Container";
-import { packages } from "@/constants";
 import Package from "./Package";
+import Menu from "./Menu";
+import { PACKAGES } from "@/constants";
 
 const Packages = () => {
-  const swiperEl = useRef();
-
   useEffect(() => {
     register();
   }, []);
 
   return (
     <Container>
-      <h2 className="text-secondary-dark text-5xl font-bold tracking-tight">
+      <h2 className="text-secondary-dark text-5xl font-bold tracking-tight text-center my-5">
         Elige un paquete para tu evento!
       </h2>
-      <swiper-container
-        ref={swiperEl}
-        loop="true"
-        slides-per-view="2"
-        space-between="4"
-      >
-        {packages.map((combo) => (
-          <swiper-slide key={combo.title}>
-            <Package
-              title={combo.title}
-              groupSize={combo.groupSize}
-              optionOne={combo.optionOne}
-              optionTwo={combo.optionTwo}
-              optionThree={combo.optionThree}
-            />
+      <Menu />
+      <swiper-container slides-per-view="3" space-between="4">
+        {PACKAGES.map(({ title, groupSize, options }) => (
+          <swiper-slide key={title}>
+            <Package title={title} groupSize={groupSize} options={options} />
           </swiper-slide>
         ))}
       </swiper-container>
