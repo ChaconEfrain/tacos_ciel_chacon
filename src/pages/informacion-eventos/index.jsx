5e66b00e3531cf256ab89f060bf33ef634a7a0cb
ChaconEfrain <efrachaga@gmail.com>
2023-04-26 13:18:23 -0600

    Add the events form page

---
+++ src/pages/informacion-eventos/index.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+const EventsForm = () => {
+  return (
+    <div className="grid grid-cols-2 max-w-5xl mx-auto py-8">
+      <form></form>
+      <figure>
+        <img
+          className=""
+          src="https://res.cloudinary.com/efrainchacon/image/upload/f_auto/v1682535610/tacos_de_canasta_ciel_chacon/Untitled-3_jict3p.png"
+          alt="Collage de personas comiendo tacos de canasta"
+        />
+      </figure>
+    </div>
+  );
+};
+
+export default EventsForm;
