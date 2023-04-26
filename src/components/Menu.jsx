5d485380278c3a574ccd4a5eb1c28d16a7cce826
ChaconEfrain <efrachaga@gmail.com>
2023-04-26 13:14:31 -0600

    Change some styles for the Menu

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -5,8 +5,8 @@ import MenuListDesktop from "./MenuListDesktop";
 const Menu = () => {
   const [menuLength] = useState(MENU.length);
   return (
-    <article className="grid grid-cols-3 justify-items-center text-xl gap-y-3">
-      <p className="col-[1/-1] justify-self-start text-2xl">
+    <article className="grid grid-cols-3 justify-items-center text-xl gap-y-4 mb-6">
+      <p className="col-[1/-1] justify-self-start text-3xl font-semibold">
         Puedes escoger hasta 4 guisos de nuestro delicioso menu
       </p>
       <MenuListDesktop menuLength={menuLength} />
