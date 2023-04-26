cf39b75c5fc3c00504364b5680b3ea11ac7b6c50
ChaconEfrain <efrachaga@gmail.com>
2023-04-26 13:15:30 -0600

    Add swiper for the events packages

--- src/components/Package.jsx
+++ src/components/Package.jsx
@@ -1,7 +1,47 @@
-import React from "react";
+import Link from "next/link";
+import { Girl, Taco } from "@/icons";
 
-const Package = ({ title, groupSize, optionOne, optionTwo, optionThree }) => {
-  return <article></article>;
+const Package = ({ title, groupSize, options }) => {
+  return (
+    <article className="text-xl relative overflow-hidden flex flex-col gap-6 border-2 rounded-lg border-secondary-medium bg ">
+      <header className="flex flex-col gap-4">
+        <span className="self-start px-6 bg-secondary-medium font-bold text-2xl text-white">
+          {groupSize}
+        </span>
+        <h3 className="text-center text-3xl font-semibold">{title}</h3>
+      </header>
+      <div className="flex flex-col p-6">
+        {options.map(({ tacos, tacosForEach, price }, i) => (
+          <section key={price} className="flex flex-col">
+            <div className="flex items-center gap-6">
+              <ul className="flex flex-col gap-4">
+                <li className="flex gap-2">
+                  <Taco className="w-6 h-6" /> {tacos}
+                </li>
+                <li className="flex gap-2">
+                  <Girl className="w-6 h-6" /> {tacosForEach}
+                </li>
+              </ul>
+              <strong className="text-5xl text-secondary-dark">{price}</strong>
+            </div>
+            <div
+              className={`bg-primary-dark h-[3px] my-6 w-3/4 self-center ${
+                i === options.length - 1 ? "hidden" : "block"
+              }`}
+            />
+          </section>
+        ))}
+      </div>
+      <footer className="flex justify-center">
+        <Link
+          href="/informacion-eventos"
+          className="bg-secondary-medium font-bold rounded-full py-2 px-10 mb-6 text-white"
+        >
+          Solicitar paquete
+        </Link>
+      </footer>
+    </article>
+  );
 };
 
 export default Package;
--- src/components/Packages.jsx
+++ src/components/Packages.jsx
@@ -1,4 +1,4 @@
-import { useEffect } from "react";
+import { useEffect, useRef } from "react";
 import { register } from "swiper/element/bundle";
 import Container from "./Container";
 import Package from "./Package";
@@ -6,23 +6,57 @@ import Menu from "./Menu";
 import { PACKAGES } from "@/constants";
 
 const Packages = () => {
+  const swiperEl = useRef(null);
+
+  const handleNextSlide = () => {
+    swiperEl.current.swiper.slideNext();
+  };
+
+  const handlePreviousSlide = () => {
+    swiperEl.current.swiper.slidePrev();
+  };
+
   useEffect(() => {
     register();
   }, []);
 
   return (
-    <Container>
-      <h2 className="text-secondary-dark text-5xl font-bold tracking-tight text-center my-5">
-        Elige un paquete para tu evento!
-      </h2>
-      <Menu />
-      <swiper-container slides-per-view="3" space-between="4">
-        {PACKAGES.map(({ title, groupSize, options }) => (
-          <swiper-slide key={title}>
-            <Package title={title} groupSize={groupSize} options={options} />
-          </swiper-slide>
-        ))}
-      </swiper-container>
+    <Container id="packages-section">
+      <div className="flex flex-col gap-6">
+        <h2 className="text-secondary-dark text-5xl font-bold tracking-tight text-center mb-4">
+          Elige un paquete para tu evento!
+        </h2>
+        <Menu />
+        <div className="flex items-center gap-4 relative">
+          <button
+            className="bg-secondary-medium w-12 h-12 absolute -left-16 rounded-full font-bold"
+            onClick={handlePreviousSlide}
+          >
+            &larr;
+          </button>
+          <swiper-container
+            ref={swiperEl}
+            slides-per-view="3"
+            space-between="16"
+          >
+            {PACKAGES.map(({ title, groupSize, options }) => (
+              <swiper-slide key={title}>
+                <Package
+                  title={title}
+                  groupSize={groupSize}
+                  options={options}
+                />
+              </swiper-slide>
+            ))}
+          </swiper-container>
+          <button
+            className="bg-secondary-medium w-12 h-12 absolute -right-16 rounded-full font-bold"
+            onClick={handleNextSlide}
+          >
+            &rarr;
+          </button>
+        </div>
+      </div>
     </Container>
   );
 };
