aaa60cf3881e7104eaf59d8d096a48b432a1b687
ChaconEfrain <efrachaga@gmail.com>
2023-04-26 13:17:54 -0600

    Remove Head component from home component

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,3 @@
-import Head from "next/head";
 import Hero from "@/components/Hero";
 import Promotions from "@/components/Promotions";
 import Packages from "@/components/Packages";
@@ -6,9 +5,6 @@ import Packages from "@/components/Packages";
 export default function Home() {
   return (
     <>
-      <Head>
-        <title>Tacos de canasta &quot;La canasta yucateca&quot;</title>
-      </Head>
       <main className="text-primary-dark font-medium">
         <Hero />
         <Promotions />
