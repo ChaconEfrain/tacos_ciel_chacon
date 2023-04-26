a6046207a39f059a1cea1fee9835cfec37a7bff7
ChaconEfrain <efrachaga@gmail.com>
2023-04-26 13:17:30 -0600

    Add Head component to the whole app

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,6 @@
 import "@/styles/globals.css";
 import { Alegreya_Sans } from "@next/font/google";
+import Head from "next/head";
 
 const alegreya = Alegreya_Sans({
   subsets: ["latin"],
@@ -9,8 +10,19 @@ const alegreya = Alegreya_Sans({
 
 export default function App({ Component, pageProps }) {
   return (
-    <div className={`${alegreya.variable} font-alegreya`}>
-      <Component {...pageProps} />
-    </div>
+    <>
+      <Head>
+        <title>Tacos de canasta &quot;La canasta yucateca&quot;</title>
+        <link
+          rel="shortcut icon"
+          href="https://res.cloudinary.com/efrainchacon/image/upload/f_auto/v1682529858/tacos_de_canasta_ciel_chacon/IMG-20230419-WA0026_1_nskbmc-removebg-preview_copyrecorte_mihnvb.png"
+        />
+      </Head>
+      <div
+        className={`${alegreya.variable} font-alegreya bg-secondary-light/70`}
+      >
+        <Component {...pageProps} />
+      </div>
+    </>
   );
 }
