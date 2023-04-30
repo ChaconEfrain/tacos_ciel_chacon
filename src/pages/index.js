7286257f5622ae999528bd0f2bd64264c86287b7
ChaconEfrain <efrachaga@gmail.com>
2023-04-30 14:12:23 -0600

    Put head in the Home component

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,4 @@
+import Head from "next/head";
 import Hero from "@/components/Hero";
 import Promotions from "@/components/Promotions";
 import Packages from "@/components/Packages";
@@ -5,6 +6,13 @@ import Packages from "@/components/Packages";
 export default function Home() {
   return (
     <>
+      <Head>
+        <title>Tacos de canasta &quot;La canasta yucateca&quot;</title>
+        <link
+          rel="shortcut icon"
+          href="https://res.cloudinary.com/efrainchacon/image/upload/f_auto/v1682529858/tacos_de_canasta_ciel_chacon/IMG-20230419-WA0026_1_nskbmc-removebg-preview_copyrecorte_mihnvb.png"
+        />
+      </Head>
       <main className="text-primary-dark font-medium">
         <Hero />
         <Promotions />
