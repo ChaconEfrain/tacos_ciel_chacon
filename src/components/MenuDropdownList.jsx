8b912fde66f102e2bb860c1c75b85d8a89c44648
ChaconEfrain <efrachaga@gmail.com>
2023-05-10 12:43:44 -0600

    Add some props to handle flavor picking

--- src/components/MenuDropdownList.jsx
+++ src/components/MenuDropdownList.jsx
@@ -1,17 +1,27 @@
 import { MENU } from "@/constants";
 import React from "react";
 
-const MenuDropdownList = ({ name, handleInputChange }) => {
+const MenuDropdownList = ({
+  name,
+  position,
+  handleInputChange,
+  selectedFlavors,
+}) => {
   return (
     <li>
       <select
         name={name}
         className="w-full focus:shadow-[0_0_0_2px_#ff7588]"
+        data-position={position}
         onChange={handleInputChange}
       >
-        <option value={false}>Seleccionar guiso</option>
+        <option value="default">Seleccionar guiso</option>
         {MENU.map((item) => (
-          <option key={item} value={item}>
+          <option
+            key={item}
+            value={item}
+            disabled={selectedFlavors.includes(item)}
+          >
             {item}
           </option>
         ))}
