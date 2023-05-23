b8bbc2e86796af6bf92e943066223ecf1c3e685a
ChaconEfrain <efrachaga@gmail.com>
2023-05-23 16:02:01 -0600

    Add whatsapp link

--- src/components/Promotion.jsx
+++ src/components/Promotion.jsx
@@ -19,7 +19,7 @@ const Promotion = ({ title, description, tacos, drinks, price, icon }) => {
       <p className="sm:text-xl text-lg mb-3">{description}</p>
       <div className="sm:text-xl text-lg flex flex-col gap-2">
         <span>Incluye:</span>
-        <div className="flex items-center lg:gap-24 sm:gap-32 gap-16">
+        <div className="grid grid-cols-2 items-center">
           <ul className="flex flex-col gap-3 ml-4">
             <li className="flex items-center gap-4">
               <Taco className="w-8 h-8" />
@@ -30,9 +30,19 @@ const Promotion = ({ title, description, tacos, drinks, price, icon }) => {
               {drinks}
             </li>
           </ul>
-          <strong className="sm:text-6xl text-5xl text-secondary-dark sm:inline-block hidden">
-            {price}
-          </strong>
+          <div className="flex flex-col gap-2 items-center">
+            <strong className="sm:text-6xl text-5xl text-secondary-dark sm:inline-block hidden">
+              {price}
+            </strong>
+            <a
+              href={`https://api.whatsapp.com/send?phone=529992787091&text=Hola! quisiera la promoción *${title}* por favor 😋`}
+              target="_blank"
+              rel="noreferrer"
+              className="bg-gradient-to-br from-secondary-medium to-secondary-dark font-bold rounded-full py-2 sm:px-16 px-4 text-white sm:text-2xl text-xl inline-block"
+            >
+              Solicitar
+            </a>
+          </div>
         </div>
       </div>
     </article>
