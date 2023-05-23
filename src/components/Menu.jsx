cfec44bb970d30fc6408a29cbc8e0cda0742c674
ChaconEfrain <efrachaga@gmail.com>
2023-05-23 12:26:49 -0600

    Change menu title

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -7,7 +7,7 @@ const Menu = () => {
   return (
     <article className="sm:text-xl text-lg mb-6 flex flex-col gap-6">
       <p className="sm:text-3xl text-2xl text-center lg:text-start font-semibold">
-        Puedes escoger hasta 4 guisos de nuestro delicioso menu
+        Puedes escoger de 4 a 6 guisos de nuestro delicioso menu
       </p>
       <MenuList menuLength={menuLength} />
     </article>
