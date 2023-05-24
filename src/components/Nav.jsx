250435fc54577806e22173a460e8e332d057827a
ChaconEfrain <efrachaga@gmail.com>
2023-05-23 18:13:32 -0600

    Add page navigation

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,7 +1,31 @@
+import { NAV_MENU } from "@/constants";
 import React from "react";
 
 const Nav = () => {
-  return <div></div>;
+  return (
+    <header className="flex justify-between absolute top-0 w-full text-secondary-light py-8 px-14 font-bold tracking-wide">
+      <img
+        src="https://res.cloudinary.com/efrainchacon/image/upload/f_auto/v1682529858/tacos_de_canasta_ciel_chacon/IMG-20230419-WA0026_1_nskbmc-removebg-preview_copyrecorte_mihnvb.png"
+        alt="Logo del negocio"
+        className="w-32 h-32"
+      />
+      <nav>
+        <ul className="flex gap-6 text-2xl">
+          {NAV_MENU.map(({ label, pointsTo, icon }) => (
+            <li key={label}>
+              {pointsTo.includes("https") ? (
+                <a href={pointsTo} target="_blank">
+                  {icon}
+                </a>
+              ) : (
+                <a href={pointsTo}>{label}</a>
+              )}
+            </li>
+          ))}
+        </ul>
+      </nav>
+    </header>
+  );
 };
 
 export default Nav;
