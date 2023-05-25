84a51ccfb3eedc5281456de2a8114eb868a1ca68
ChaconEfrain <efrachaga@gmail.com>
2023-05-25 16:34:59 -0600

    Improve menu

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -1,15 +1,21 @@
-import { MENU } from "@/constants";
 import React, { useState } from "react";
-import MenuList from "./MenuList";
+import { MENU } from "@/constants";
+import { Check } from "@/icons";
 
 const Menu = () => {
   const [menuLength] = useState(MENU.length);
   return (
-    <article className="sm:text-xl text-lg mb-6 flex flex-col gap-6">
-      <p className="sm:text-3xl text-2xl text-center lg:text-start font-semibold">
+    <article className="sm:text-xl text-lg mb-6 flex flex-col items-center gap-6">
+      <p className="sm:text-3xl text-2xl text-center lg:text-start font-semibold self-start">
         Puedes escoger de 4 a 6 guisos de nuestro delicioso menu
       </p>
-      <MenuList menuLength={menuLength} />
+      <ul className="grid lg:grid-cols-3 md:grid-cols-2 gap-x-24 gap-y-2">
+        {MENU.map((item) => (
+          <li key={item} className="flex items-center gap-2">
+            <Check className="sm:w-6 w-4 sm:h-6 h-4" /> {item}
+          </li>
+        ))}
+      </ul>
     </article>
   );
 };
